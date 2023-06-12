fa4c1fc0bbcf5a98851da7f77226557e0c88e0d8
Apple <akash.chauhan@appventurez.com>
2023-06-12 17:22:46 +0530

    changes

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "static")));
 app.use("/peerjs", peerServer);
 
-app.get("/", (req, res) => {
-    res.send({ success: true });
-});
+// app.get("/", (req, res) => {
+//     res.send({ success: true });
+// });
 
-app.get("/web-rtc", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
